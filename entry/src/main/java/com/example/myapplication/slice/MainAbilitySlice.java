0a3ce8ada3110a6af33679be235cecdf7177d992
Chanukolli <kollichanu@gmail.com>
2022-01-24 11:14:46 +0530

    changed code

--- entry/src/main/java/com/example/myapplication/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/example/myapplication/slice/MainAbilitySlice.java
@@ -33,13 +33,13 @@ public class MainAbilitySlice extends AbilitySlice {
         super.setUIContent(ResourceTable.Layout_ability_main);
 
         SocailConnect s = (SocailConnect) findComponentById(ResourceTable.Id_image1);
-        s.setContext(this,"https://www.instagram.com/chanakya_raghavendra/");
+        s.setContext(this, "https://www.instagram.com/chanakya_raghavendra/");
         SocailConnect s1 = (SocailConnect) findComponentById(ResourceTable.Id_image2);
-        s1.setContext(this,"https://www.instagram.com/chanakya_raghavendra/");
+        s1.setContext(this, "https://www.instagram.com/chanakya_raghavendra/");
         SocailConnect s2 = (SocailConnect) findComponentById(ResourceTable.Id_image3);
-        s2.setContext(this,"https://www.instagram.com/chanakya_raghavendra/");
+        s2.setContext(this, "https://www.instagram.com/chanakya_raghavendra/");
         SocailConnect s3 = (SocailConnect) findComponentById(ResourceTable.Id_image4);
-        s3.setContext(this,"https://www.instagram.com/chanakya_raghavendra/");
+        s3.setContext(this, "https://www.instagram.com/chanakya_raghavendra/");
     }
 
     @Override
